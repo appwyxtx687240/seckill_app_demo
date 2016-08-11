0c0311e6158372ca495e86cada063649bf0e8fc6
Summer <zhangjian@chinamobo.com>
2016-08-11 15:16:51 +0800

    创建app demo

---
+++ SeckillDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeckillDemo
+//
+//  Created by Summer on 16/8/11.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeckillDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeckillDemo
+//
+//  Created by Summer on 16/8/11.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
