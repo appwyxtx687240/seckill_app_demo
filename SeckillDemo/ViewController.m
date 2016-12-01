1efeabac295cd9e40bf6ea49af874154d63754cf
张健  ZJ <zhangjian@chinamobo.com>
2016-12-01 21:25:07 +0800

    Revert "秒杀商品列表接口测试通过"
    This reverts commit 15428f263fb51523261ed58e08d63e29138226b8.

--- SeckillDemo/ViewController.m
+++ SeckillDemo/ViewController.m
@@ -16,28 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = CGRectMake(100, 200, 200, 200);
-    [btn addTarget:self action:@selector(downloadJson) forControlEvents:UIControlEventTouchUpInside];
-    [btn setTitle:@"下载json数据" forState:UIControlStateNormal];
-    btn.backgroundColor = [UIColor greenColor];
-
-    [self.view addSubview:btn];
-
-   }
-
--(void)downloadJson
-{
-    NSString *localhost = @"http://192.168.1.229:8080/seckill/list";
-
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:localhost]completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSString *json = [NSJSONSerialization JSONObjectWithData:data options:nil error:nil];
-        NSLog(@"json:%@", json);
-    }];
-    [dataTask resume];
+    // Do any additional setup after loading the view, typically from a nib.
 }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
